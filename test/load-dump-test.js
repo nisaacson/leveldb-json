226b4cc913297befcd69674e6acea7477b370f28
Noah Isaacson <noah@nisaacson.com>
2014-04-25 17:58:26 -0600

    Create db on the fly

--- test/load-dump-test.js
+++ test/load-dump-test.js
@@ -4,12 +4,13 @@ var chai = require('chai')
 var expect = chai.expect
 var createDB =  require('../lib/db')
 var load = require('../lib/load')
+var dump = require('../lib/dump')
+var dbFilePath = path.join(__dirname, 'tmp', 'db')
 
+var db = createDB(dbFilePath)
 describe('Load from File', function() {
 
-  it('should load correctly', function(done) {
-    var dbFilePath = path.join(__dirname, 'tmp', 'db')
-    var db = createDB(dbFilePath)
+  it('should load and dump correctly', function(done) {
     var file = path.join(__dirname, 'data', 'test.json')
     var loadStream = load(file, db)
     var loadedSpy = sinon.spy(loaded)
@@ -20,6 +21,22 @@ describe('Load from File', function() {
     function loaded() { }
     function validate() {
       expect(loadedSpy.callCount).to.equal(2)
+      done()
+    }
+  })
+
+  it('should dump correctly', function(done) {
+    var file = path.join(__dirname, 'tmp', 'dump.json')
+    var dumpStream = dump(file, db)
+    var dumpedSpy = sinon.spy(dumped)
+
+    dumpStream.on('dumped', dumpedSpy)
+    dumpStream.on('finish', validate)
+
+    function dumped() { }
+
+    function validate() {
+      expect(dumpedSpy.callCount).to.equal(2)
       done()
     }
 
